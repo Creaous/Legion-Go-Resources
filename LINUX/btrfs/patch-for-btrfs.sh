e947e8b00d2776a587e17980c3abffbfb4da3c00
Mitchell <68230846+Creaous@users.noreply.github.com>
2023-12-20 00:24:16 +1030

    Upload patch-for-btrfs.sh

---
+++ LINUX/btrfs/patch-for-btrfs.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Function for downloading and applying patches
+download_and_apply_patch() {
+    patch_url=$1
+    patch_filename=$(basename "${patch_url}")
+    target_file="/usr/lib/hwsupport/${patch_filename%.patch}"
+
+    echo "Downloading patch: ${patch_filename}"
+    curl -sSL "${patch_url}" -o "${patch_filename}"
+
+    if [ $? -eq 0 ]; then
+        echo "Applying patch to ${target_file}"
+        patch -u ${target_file} -i ${patch_filename}
+        rm "${patch_filename}"
+    else
+        echo "Failed to download patch: ${patch_filename}"
+    fi
+}
+
+# Ensure target directory exists
+if [ ! -d "/usr/lib/hwsupport" ]; then
+    echo "Creating target directory: /usr/lib/hwsupport"
+    sudo mkdir -p "/usr/lib/hwsupport"
+fi
+
+# Download and apply patches
+download_and_apply_patch "https://gitlab.com/popsulfr/steamos-btrfs/-/raw/main/files/usr/lib/hwsupport/format-sdcard.sh.patch"
+download_and_apply_patch "https://gitlab.com/popsulfr/steamos-btrfs/-/raw/main/files/usr/lib/hwsupport/sdcard-mount.sh.patch"
